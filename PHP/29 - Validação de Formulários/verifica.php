1a99a1a313ed3b88eb6f0e89030da34885f09f6d
xOmilk <leite46052@gmail.com>
2025-08-30 23:15:54 -0300

    Implementação da validação de CPF com dígitos verificadores

--- PHP/29 - Validação de Formulários/verifica.php
+++ PHP/29 - Validação de Formulários/verifica.php
@@ -1,6 +1,39 @@
 <?php
 session_start();
 
+// Função para validar CPF com dígitos verificadores
+function validarCPF($cpf)
+{
+    // Remove caracteres não numéricos
+    $cpf = preg_replace('/[^0-9]/', '', $cpf);
+
+    // Verifica se tem 11 dígitos
+    if (strlen($cpf) != 11) {
+        return false;
+    }
+
+    // Verifica se todos os dígitos são iguais (ex: 111.111.111-11)
+    if (preg_match('/(\d)\1{10}/', $cpf)) {
+        return false;
+    }
+
+    $soma = 0;
+    for ($i = 0; $i < 9; $i++) {
+        $soma += $cpf[$i] * (10 - $i);
+    }
+    $resto = $soma % 11;
+    $digito1 = ($resto < 2) ? 0 : 11 - $resto;
+
+    $soma = 0;
+    for ($i = 0; $i < 10; $i++) {
+        $soma += $cpf[$i] * (11 - $i);
+    }
+    $resto = $soma % 11;
+    $digito2 = ($resto < 2) ? 0 : 11 - $resto;
+
+    // Verifica se os dígitos calculados são iguais aos informados
+    return ($cpf[9] == $digito1 && $cpf[10] == $digito2);
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
@@ -22,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // array de erros
     $erros = [];
 
-    if (empty($data['nome'])) {
+    if (empty($data['nome']) || trim($data['nome'] === '')) {
         $erros[] = "Nome é obrigatório";
     }
 
@@ -32,6 +65,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($data['senha'] !== $data['confirma_senha']) {
         $erros[] = "Senhas não coincidem";
+    }
+
+    // Validação do CPF
+    if (empty($data['cpf'])) {
+        $erros[] = "CPF é obrigatório";
+    } elseif (!validarCPF($data['cpf'])) {
+        $erros[] = "CPF inválido - dígitos verificadores incorretos";
     }
 
     //
